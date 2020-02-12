cfc7d789beed320c881299764bc114814d456559
Alexandremma <46142888+Alexandremma@users.noreply.github.com>
2020-02-12 00:26:55 -0300

    primeiro commit - início projeto api em springboot

---
+++ src/main/java/br/com/treinaweb/springbootapi/entity/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.springbootapi.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullabel = false)
+    private String nome;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/main/java/br/com/treinaweb/springbootapi/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.springbootapi.repository;
+
+import br.com.treinaweb.springbootapi.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+
+
+}
